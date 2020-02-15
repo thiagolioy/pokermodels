474656cb3bd157c0b0585c83edeccf0fc7cfff38
Thiago Lioy <thiagolioy@gmail.com>
2020-02-15 13:12:49 -0300

    Initial Commit

---
+++ PokerModels/PokerModels.h
@@ -0,0 +1,19 @@
+//
+//  PokerModels.h
+//  PokerModels
+//
+//  Created by Thiago Lioy on 15/02/20.
+//  Copyright © 2020 Thiago Lioy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PokerModels.
+FOUNDATION_EXPORT double PokerModelsVersionNumber;
+
+//! Project version string for PokerModels.
+FOUNDATION_EXPORT const unsigned char PokerModelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokerModels/PublicHeader.h>
+
+
